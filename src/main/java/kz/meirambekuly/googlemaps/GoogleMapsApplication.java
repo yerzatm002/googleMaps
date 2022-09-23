b66f386b5da6d2b79e23a36d936e6d64c500327b
yerzat <erzatnis@gmail.com>
2022-09-23 22:04:28 +0600

    first commit

---
+++ src/main/java/kz/meirambekuly/googlemaps/GoogleMapsApplication.java
@@ -0,0 +1,13 @@
+package kz.meirambekuly.googlemaps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleMapsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoogleMapsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/meirambekuly/googlemaps/GoogleMapsApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.meirambekuly.googlemaps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleMapsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
